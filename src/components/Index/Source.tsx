dcd13793cecdb3718a8731015428b8c824187358
Robby Fairclough <robby.fairclough@gmail.com>
2022-04-22 04:52:59 -0700

    social

--- src/components/Index/Source.tsx
+++ src/components/Index/Source.tsx
@@ -1,3 +1,5 @@
+import { faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { FC, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -13,6 +15,23 @@ const Source: FC = () => {
           Souce code
         </button>
       </a>
+      <ul>
+        <li>
+          <a href="https://twitter.com/airclough" target="_blank">
+            <FontAwesomeIcon icon={ faTwitter } />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/robby.fairclough/" target="_blank">
+            <FontAwesomeIcon icon={ faInstagram } />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/robby-fairclough/" target="_blank">
+            <FontAwesomeIcon icon={ faLinkedin } />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -1,5 +1,8 @@
+import { config, dom } from '@fortawesome/fontawesome-svg-core';
 import NextHead from 'next/head';
 import React, { FC } from 'react';
+
+config.autoAddCss = false;
 
 const Head: FC = () => (
   <NextHead>
@@ -7,6 +10,7 @@ const Head: FC = () => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <style>{ dom.css() }</style>
   </NextHead>
 );
 
