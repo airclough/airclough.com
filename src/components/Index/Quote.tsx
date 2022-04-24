f95f23de13b3bf7d155d9140f6385ffaf0f9d966
Robby Fairclough <robby.fairclough@gmail.com>
2022-04-24 00:09:35 -0700

    fix quote

--- src/components/Index/Quote.tsx
+++ src/components/Index/Quote.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
 
-const Quote: FC = () => {
-  return (
-    <div className="Quote">
-      <p>No bird soars too high,</p>
-      <p>if he soars with his own wings</p>
-    </div>
-  );
-};
+const Quote: FC = () => (
+  <div className="Quote">
+    <p>No bird soars too high,</p>
+    <p>if he soars with his own wings</p>
+  </div>
+);
 
 export default Quote;
