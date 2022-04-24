c9b58e581d93b2aeaf2a23fbfaabc2a3f0ec3e15
Robby Fairclough <robby.fairclough@gmail.com>
2022-04-23 20:47:45 -0700

    remove selector

--- src/components/Index/Quote.tsx
+++ src/components/Index/Quote.tsx
@@ -1,11 +1,6 @@
 import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
-
-import { RootState } from '../../redux/reducers';
 
 const Quote: FC = () => {
-  const { namesakeTransition } = useSelector( ( { app }: RootState ) => app );
-
   return (
     <div className="Quote">
       <p>No bird soars too high,</p>
