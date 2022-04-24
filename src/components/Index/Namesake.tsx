25c7b5004b3bb200c70480cefb3211e7382a5500
Robby Fairclough <robby.fairclough@gmail.com>
2022-04-24 00:03:07 -0700

    mobile styles

--- src/components/Index/Namesake.tsx
+++ src/components/Index/Namesake.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Air from './Air';
@@ -7,6 +7,7 @@ import { SET_NAMESAKE, SET_NAMESAKE_TRANSITION } from '../../redux/actions/app';
 import { RootState } from '../../redux/reducers';
 
 interface LettersProps {
+  multiplier: number;
   namesakeLetter: string;
 }
 
@@ -14,8 +15,8 @@ const alphabetMinusQ = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'.split( '' );
 const clough = 'CLOUGH'.split( '' );
 const interval = 250;
 
-const Letters: FC<LettersProps> = ( { namesakeLetter } ) => {
-  const y = alphabetMinusQ.indexOf( namesakeLetter ) * 8;
+const Letters: FC<LettersProps> = ( { multiplier, namesakeLetter } ) => {
+  const y = alphabetMinusQ.indexOf( namesakeLetter ) * multiplier;
 
   return (
     <div className="Letters">
@@ -32,7 +33,17 @@ const Letters: FC<LettersProps> = ( { namesakeLetter } ) => {
 
 const Namesake: FC = () => {
   const { airTransition, namesake } = useSelector( ( { app }: RootState ) => app );
+  const [ multiplier, setMultiplier ] = useState<number>( 8 );
   const dispatch = useDispatch();
+
+  useEffect( () => {
+    if ( typeof window === 'undefined' ) return;
+    setMultiplier( window.innerWidth > 1024 ? 8 : 2 );
+
+    window.addEventListener( 'resize', () => {
+      setMultiplier( window.innerWidth > 1024 ? 8 : 2 );
+    } );
+  }, [] );
 
   useEffect( () => {
     if ( airTransition !== 'COMPLETE' ) return;
@@ -54,10 +65,10 @@ const Namesake: FC = () => {
 
   return (
     <div className="Namesake">
-      <h1 className={ ( airTransition === 'COMPLETE' && 'show' ) || '' }>
-        { namesake.map( ( namesakeLetter, i ) => <Letters key={ i } namesakeLetter={ namesakeLetter } /> ) }
-      </h1>
       <Air />
+      <h1 className={ ( airTransition === 'COMPLETE' && 'show' ) || '' }>
+        { namesake.map( ( namesakeLetter, i ) => <Letters key={ i } multiplier={ multiplier } namesakeLetter={ namesakeLetter } /> ) }
+      </h1>
       <JordanLogo />
     </div>
   );
