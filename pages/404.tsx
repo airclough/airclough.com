ce256a3078cb8c0c63ce2746786a2c2ae1bfa90a
Robby Fairclough <robby.fairclough@gmail.com>
2022-05-15 16:36:32 -0700

    404

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const NotFound: FC = () => (
+  <div className="NotFound">
+    <div className="NotFoundContainer">
+      <img alt="Jordan crying" src="/images/jordan-crying.png" />
+      <a href="/">Home</a>
+    </div>
+  </div>
+);
+
+export default NotFound;
