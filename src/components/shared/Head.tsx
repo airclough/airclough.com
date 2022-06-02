c35955842cd85ef63af9279e302407e9f77fe15c
Robby Fairclough <robby.fairclough@gmail.com>
2022-06-02 10:30:13 -0700

    fix script

--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -9,17 +9,20 @@ config.autoAddCss = false;
 const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
 const Head: FC = () => (
-  <NextHead>
-    <title>airclough.com | Robby Fairclough</title>
-    <meta name="description" content="Engineered to the exact specifications of championship athletes." />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta property="og:title" content="airclough.com" />
-    <meta property="og:description" content="Engineered to the exact specifications of championship athletes." />
-    <meta property="og:url" content="https://airclough.com" />
-    <meta property="og:type" content="website" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <style>{ dom.css() }</style>
+  <>
+    <NextHead>
+      <title>airclough.com | Robby Fairclough</title>
+      <meta content="airclough.com | Robby Fairclough" key="title" property="og:title" />
+      <meta name="description" content="Engineered to the exact specifications of championship athletes." />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content="airclough.com" />
+      <meta property="og:description" content="Engineered to the exact specifications of championship athletes." />
+      <meta property="og:url" content="https://airclough.com" />
+      <meta property="og:type" content="website" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <style>{ dom.css() }</style>
+    </NextHead>
     <Script
       id="googleTagManager"
       onLoad={ () => {
@@ -31,7 +34,7 @@ const Head: FC = () => (
       src={ `https://www.googletagmanager.com/gtag/js?id=${ googleAnalyticsId }` }
       strategy="afterInteractive"
     />
-  </NextHead>
+  </>
 );
 
 export default Head;
