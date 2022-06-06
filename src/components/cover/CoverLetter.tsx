2a6942e3b389d348070b932b74c9537b87d8855b
Robby Fairclough <robby.fairclough@gmail.com>
2022-06-06 14:54:44 -0700

    facts

--- src/components/cover/CoverLetter.tsx
+++ src/components/cover/CoverLetter.tsx
@@ -6,7 +6,7 @@ import { useAppSelector } from '../../redux/hooks';
 const CoverLetter: FC = () => {
   const { company } = useAppSelector( ( { cover } ) => cover );
   const { fields } = company;
-  const { Cover, Logo, Name } = fields;
+  const { Facts, Logo, Name } = fields;
   const dropboxLink = fields[ 'Cover Letter Dropbox Link' ];
   const [ image ] = Logo;
   const { url } = image;
@@ -17,7 +17,7 @@ const CoverLetter: FC = () => {
         <img src={ url } />
       </div>
       <p>Welcome <span>{ Name }</span>, and thanks for stopping by my site. The purpose of this page is to act as an extension of my <a href={ dropboxLink } target="_blank">cover letter</a> in order to show off my past projects and my current interests in software development.</p>
-      { Cover && <p /> }
+      { Facts && <p>{ Facts }</p> }
       <p>As noted on my resume, my previous position was as the CTO of Neyborly. I wrote code that ran everywhere from the AWS cloud to Nvidia hardware devices. <a href="/work/neyborly">Here</a> is just a small sample of projects I worked on during my time at Neyborly.</p>
       <p>Since leaving Neyborly in April, I have spent time upping my code game and adding a few new skills to my repertoire. One such skill being blockchain development. The Ethereum blockchain and Solidity programming language to be exact. Below is a game I coded called Monument Park, a homage to the greatest baseball players to ever lace up their spikes. It combines my new found blockchain abilities and my love for the New York Yankees. Also, If you have a Spotify account, feel free to check out my <a href="/">home page</a> where I created a custom Spotify player.</p>
       <p>All source code is up on <a href="https://github.com/airclough/airclough.com" target="_blank">Github</a>.</p>
